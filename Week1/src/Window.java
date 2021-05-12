6ae605cffa26664f62ec856d8cfa84893c43d1ff
josph <josph@josph-HP-15-Notebook-PC>
2021-05-12 18:50:15 -0400

    Test Jframe

--- Week1/src/Window.java
+++ Week1/src/Window.java
@@ -1,10 +1,11 @@
-
+import javax.swing.*;
 public class Window {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		System.out.println("Test");
+		JFrame serte = new JFrame();
 
 	}
 
