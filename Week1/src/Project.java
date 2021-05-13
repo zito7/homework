94f3f2512df4d0bb6d82fafa3c7c78cf974c6c78
josph <josph@josph-HP-15-Notebook-PC>
2021-05-13 18:18:28 -0400

    Processus de positionnement des composants et creation de fenetre

--- Week1/src/Project.java
+++ Week1/src/Project.java
@@ -4,7 +4,11 @@ public class Project {
 
 	public static void main(String[] args) {
 		Window screen = new Window();
-		
+		 SwingUtilities.invokeLater(new Runnable() {   public void run() {  
+			 new Window();  
+			 }  
+			 });  
+
 
 	}
 
--- Week1/src/Window.java
+++ Week1/src/Window.java
@@ -1,3 +1,6 @@
+
+import java.awt.*;
+
 import javax.swing.*;
 public class Window {
 
@@ -6,9 +9,51 @@ Window(){
 	//instanciation de ma fenetre
 	JFrame fenetre = new JFrame();
 	fenetre.setSize(500, 600);
-	fenetre.setName("Bienvenue");
+	fenetre.setTitle("Bienvenue");
 	fenetre.setLocationRelativeTo(null);
 	fenetre.setVisible(true);
+	fenetre.setLayout(null);
+
+
+	fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//Creation label et Champ texte
+	JLabel lb1 = new JLabel("Entrer un premier nombre:");
+	JTextField texte1= new JTextField();
+	lb1.setBounds(40, 60, 150, 90);
+	
+	texte1.setBounds(260,60,100,60);
+	fenetre.getContentPane().add(lb1);
+	fenetre.getContentPane().add(texte1);
+	JLabel lb2 = new JLabel("Entrer un deuxieme nombre:");
+	JTextField texte2= new JTextField();
+	lb2.setBounds(40, 200, 150, 90);
+	texte2.setBounds(80,200,100,60);
+	fenetre.getContentPane().add(lb2);
+	fenetre.getContentPane().add(texte2);
+	
+
+
+	
+	//Creation des boutons
+	JButton bt1= new JButton("additionner");
+	bt1.setBounds(60,300,100,60);
+
+	fenetre.getContentPane().add(bt1);
+	JButton bt2= new JButton("soustraire");
+	bt2.setBounds(120,300,100,60);
+
+	fenetre.getContentPane().add(bt2);
+	JButton bt3= new JButton("diviser");
+	bt3.setBounds(180,300,100,60);
+
+	fenetre.getContentPane().add(bt3);
+	JButton bt4= new JButton("multiplier");
+	bt3.setBounds(240,300,100,60);
+
+	fenetre.getContentPane().add(bt4);
+	
+	
+	
 	
 	
 }
