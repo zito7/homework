b90c1a31b87533eaec9c017b4aa622027ff9df8d
josph <josph@josph-HP-15-Notebook-PC>
2021-05-13 18:40:39 -0400

    Programme termine

--- Week1/src/Window.java
+++ Week1/src/Window.java
@@ -8,7 +8,7 @@ public class Window {
 Window(){
 	//instanciation de ma fenetre
 	JFrame fenetre = new JFrame();
-	fenetre.setSize(500, 600);
+	fenetre.setSize(500, 500);
 	fenetre.setTitle("Bienvenue");
 	fenetre.setLocationRelativeTo(null);
 	fenetre.setVisible(true);
@@ -19,15 +19,15 @@ Window(){
 	//Creation label et Champ texte
 	JLabel lb1 = new JLabel("Entrer un premier nombre:");
 	JTextField texte1= new JTextField();
-	lb1.setBounds(40, 60, 150, 90);
+	lb1.setBounds(40, 60, 250, 90);
 	
-	texte1.setBounds(260,60,100,60);
+	texte1.setBounds(260,90,60,40);
 	fenetre.getContentPane().add(lb1);
 	fenetre.getContentPane().add(texte1);
 	JLabel lb2 = new JLabel("Entrer un deuxieme nombre:");
 	JTextField texte2= new JTextField();
-	lb2.setBounds(40, 200, 150, 90);
-	texte2.setBounds(80,200,100,60);
+	lb2.setBounds(40, 200, 250, 90);
+	texte2.setBounds(260,230,60,40);
 	fenetre.getContentPane().add(lb2);
 	fenetre.getContentPane().add(texte2);
 	
@@ -35,23 +35,26 @@ Window(){
 
 	
 	//Creation des boutons
-	JButton bt1= new JButton("additionner");
-	bt1.setBounds(60,300,100,60);
+	JButton bt1= new JButton("addtn");
+	bt1.setBounds(40,300,100,60);
 
 	fenetre.getContentPane().add(bt1);
-	JButton bt2= new JButton("soustraire");
-	bt2.setBounds(120,300,100,60);
+	JButton bt2= new JButton("sstr");
+	bt2.setBounds(155,300,100,60);
 
 	fenetre.getContentPane().add(bt2);
-	JButton bt3= new JButton("diviser");
-	bt3.setBounds(180,300,100,60);
+	JButton bt3= new JButton("dvsr");
+	bt3.setBounds(265,300,100,60);
 
 	fenetre.getContentPane().add(bt3);
-	JButton bt4= new JButton("multiplier");
-	bt3.setBounds(240,300,100,60);
+	JButton bt4= new JButton("mtplr");
+	bt4.setBounds(375,300,100,60);
 
 	fenetre.getContentPane().add(bt4);
 	
+    JLabel lb3 = new JLabel("RESULTAT:");
+    lb3.setBounds(40, 400, 250, 90);
+    fenetre.getContentPane().add(lb3);
 	
 	
 	
